163c20f87b0e64955d55bf260f046ffebdd5893c
BSS-Group <bss-group@Macs-MBP.lan>
2022-12-13 14:24:01 +0700

    gitignore

--- src/customize/catalog/components/categories/index.js
+++ src/customize/catalog/components/categories/index.js
@@ -61,7 +61,7 @@ class Categories extends React.Component {
                             return null;
                         }
                     }}
-                    />
+                />
 
             </View>
         );
